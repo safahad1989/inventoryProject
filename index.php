ea706e28b28d081bda9316f2e5a84e7e81567635
safahad1989 <fahad.bd@hotmail.com>
2022-09-02 19:55:57 +0600

    This is new update

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<html>
+ <head>
+   <tittle> Wellcome</tittle>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/style.css" />
+ </head>
+  <body>
+    <div>
+	  <p>Wellcome to your inventory Boss</p>
+	</div>
+	<div class="call-sm-6">
+	 <a href="login.php">
+	   <input type="button" class="btn btn danger floid-right" value="Login" />
+	 </a>
+	
+	</div>
+	<div class="call-sm-6">
+	 <a href="register.php">
+	   <input type="button" class="btn btn danger floid-left" value="Login" />
+	 </a>
+	
+	</div>
+    
+  
+  
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+  </body>
+
+</html>
